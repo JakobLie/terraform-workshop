6ae3ebc8ea68a3f6e24b06e0ea59ac686e15cc9b
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-10-18 20:11:13 +0800

    as

--- section2/terraform/main.tf
+++ section2/terraform/main.tf
@@ -1,5 +1,6 @@
 # Uncomment this to deploy terraform
 resource "aws_instance" "app_server" {
+  
   # specify arguments here
   ami           = "ami-0ad522a4a529e7aa8" # EC2 AMI
   instance_type = "t3.small"              # EC2 Instance Type
